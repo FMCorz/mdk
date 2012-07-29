4c3ed8e86ade89966b0795e09330b63e2ff2ab02
Fred <fmcell@gmail.com>
2012-07-29 23:22:37 +0800

    Function to call an external process

--- lib/tools.py
+++ lib/tools.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import shlex
+import subprocess
+
 def debug(str):
 	print str
+
+def process(cmd, cwd=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE):
+	if type(cmd) != 'list':
+		cmd = shlex.split(str(cmd))
+	proc = subprocess.Popen(cmd, cwd=cwd, stdout=stdout, stderr=stderr)
+	(out, err) = proc.communicate()
+	return (proc.returncode, out, err)
