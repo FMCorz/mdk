8aa502733b80e5cb81a1d43a9edb79bc84ba71b4
Frederic Massart <fred@moodle.com>
2012-08-10 17:01:39 +0800

    Licensed headers in place

--- moodle-install.py
+++ moodle-install.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Moodle Development Kit
+
+Copyright (c) 2012 Frédéric Massart - FMCorz.net
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+http://github.com/FMCorz/mdk
+"""
 import sys
 import os
 import argparse
@@ -15,8 +35,7 @@ Wp = workplace.Workplace()
 # Arguments
 parser = argparse.ArgumentParser(description='Install a Moodle instance')
 parser.add_argument('-e', '--engine', action='store', choices=['mysqli', 'pgsql'], default=C('defaultEngine'), help='database engine to use', metavar='engine')
-parser.add_argument('-y', '--non-interactive', action='store_true', help='non-interactive mode', dest='noninteractive')
-parser.add_argument('-f', '--fullname', action='store', help='full name of the instance')
+parser.add_argument('-f', '--fullname', action='store', help='full name of the instance', metavar='fullname')
 parser.add_argument('name', metavar='name', default=None, nargs='?', help='name of the instance')
 args = parser.parse_args()
 
