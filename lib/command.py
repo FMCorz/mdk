3fdb1a86d824562245c62ad2018d8deb97864c82
Fred <fmcell@gmail.com>
2013-03-01 08:21:26 +0800

    First step towards command abstraction

---
+++ lib/command.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Moodle Development Kit
+
+Copyright (c) 2013 Frédéric Massart - FMCorz.net
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+http://github.com/FMCorz/mdk
+"""
+
+import argparse
+
+
+class Command(object):
+    """Represents a command"""
+
+    _arguments = [
+            (
+                ['foo'],
+                {
+                    'help': 'I\'m an argument'
+                }
+            ),
+            (
+                ['-b', '--bar'],
+                {
+                    'action': 'store_true',
+                    'help': 'I\'m a flag'
+                }
+            )
+        ]
+    _description = 'Undocumented command'
+
+    def __init__(self, config):
+        self.C = config
+
+    @property
+    def arguments(self):
+        return self._arguments
+
+    @property
+    def description(self):
+        return self._description
+
+    def run(self, args):
+        return True
+
+
+class RunCommand(object):
+    """Executes a command"""
+
+    def __init__(self, command):
+        self._command = command
+
+    @property
+    def command(self):
+        return self._command
+
+    def run(self):
+        parser = argparse.ArgumentParser(description=self.command.description)
+        for argument in self.command.arguments:
+            args = argument[0]
+            kwargs = argument[1]
+            if 'sub-commands' in kwargs:
+                subs = kwargs['sub-commands']
+                del kwargs['sub-commands']
+                subparsers = parser.add_subparsers(**kwargs)
+                for name, sub in subs.items():
+                    subparser = subparsers.add_parser(name, **sub[0])
+                    defaults = {args[0]: name}
+                    subparser.set_defaults(**defaults)
+                    for subargument in sub[1]:
+                        sargs = subargument[0]
+                        skwargs = subargument[1]
+                        subparser.add_argument(*sargs, **skwargs)
+            else:
+                parser.add_argument(*args, **kwargs)
+        args = parser.parse_args()
+        self.command.run(args)
+
+
+if __name__ == "__main__":
+    RunCommand(Command()).run()
---
+++ lib/commands/alias.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Moodle Development Kit
+
+Copyright (c) 2013 Frédéric Massart - FMCorz.net
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+http://github.com/FMCorz/mdk
+"""
+
+import argparse
+from lib.command import Command
+
+
+class AliasCommand(Command):
+
+    _arguments = [
+        (
+            ['action'],
+            {
+                'metavar': 'action',
+                'help': 'the action to perform',
+                'sub-commands':
+                    {
+                        'list': (
+                            {
+                                'help': 'list the aliases'
+                            },
+                            []
+                        ),
+                        'show': (
+                            {
+                                'help': 'display an alias'
+                            },
+                            [
+                                (
+                                    ['alias'],
+                                    {
+                                        'type': str,
+                                        'metavar': 'alias',
+                                        'default': None,
+                                        'help': 'alias to display'
+                                    }
+                                )
+                            ]
+                        ),
+                        'show': (
+                            {
+                                'help': 'display an alias'
+                            },
+                            [
+                                (
+                                    ['alias'],
+                                    {
+                                        'type': str,
+                                        'metavar': 'alias',
+                                        'default': None,
+                                        'help': 'alias to display'
+                                    }
+                                )
+                            ]
+                        ),
+                        'add': (
+                            {
+                                'help': 'adds an alias'
+                            },
+                            [
+                                (
+                                    ['alias'],
+                                    {
+                                        'type': str,
+                                        'metavar': 'alias',
+                                        'default': None,
+                                        'help': 'alias to display'
+                                    }
+                                ),
+                                (
+                                    ['definition'],
+                                    {
+                                        'type': str,
+                                        'metavar': 'command',
+                                        'default': None,
+                                        'nargs': argparse.REMAINDER,
+                                        'help': 'alias definition'
+                                    }
+                                )
+                            ]
+                        ),
+                        'remove': (
+                            {
+                                'help': 'remove an alias'
+                            },
+                            [
+                                (
+                                    ['alias'],
+                                    {
+                                        'type': str,
+                                        'metavar': 'alias',
+                                        'default': None,
+                                        'help': 'alias to remove'
+                                    }
+                                )
+                            ]
+                        )
+                    }
+            }
+        )
+    ]
+    _description = 'Manage your aliases'
+
+    def run(self, args):
+        if args.action == 'list':
+            aliases = self.C.get('aliases')
+            for alias, command in aliases.items():
+                print '{0:<20}: {1}'.format(alias, command)
+
+        elif args.action == 'show':
+            alias = self.C.get('aliases.%s' % args.alias)
+            if alias != None:
+                print alias
+
+        elif args.action == 'add':
+            self.C.add('aliases.%s' % args.alias, ' '.join(args.definition))
+
+        elif args.action == 'remove':
+            self.C.remove('aliases.%s' % args.alias)
