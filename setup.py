6774ee4cb2cc86e6a04aa310cefbb3fe33f3daa3
Frederic Massart <fred@moodle.com>
2014-07-22 14:49:07 +0800

    Finalising the python package

---
+++ setup.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Moodle Development Kit
+
+Copyright (c) 2014 Frédéric Massart - FMCorz.net
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+http://github.com/FMCorz/mdk
+"""
+
+import os
+from setuptools import setup, find_packages
+
+# Load version number.
+execfile('mdk/version.py')
+
+# Load the requirements.
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+# Get the content of the scripts folder.
+scripts = []
+for f in os.listdir(os.path.join(os.path.dirname(__file__), 'mdk', 'scripts')):
+    if f == 'README.rst':
+        continue
+    scripts.append('mdk/scripts/%s' % (f))
+
+# Get the content of the extra folder.
+tools = []
+for f in os.listdir(os.path.join(os.path.dirname(__file__), 'extra')):
+    tools.append('extra/%s' % (f))
+
+setup(
+    name='moodle-sdk',
+    version=__version__,
+    description='Moodle Development Kit',
+    license='MIT',
+
+    url='https://github.com/FMCorz/mdk',
+    author='Frédéric Massart',
+    author_email='fred@fmcorz.net',
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Education',
+        'Topic :: Software Development',
+        'Topic :: Utilities'
+    ],
+    keywords='mdk moodle moodle-sdk',
+
+    packages=find_packages(),
+    package_data={'mdk': ['config-dist.json']},
+    install_requires=requirements,
+    data_files=[
+        ('scripts', scripts),
+        ('tools', tools)
+    ],
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': [
+            'mdk = mdk.__main__:main'
+        ]
+    }
+)
