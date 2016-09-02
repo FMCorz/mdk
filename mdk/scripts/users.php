b6cbe350ad7ff3d875b0aa384e8e1a213bacb6c7
Frederic Massart <fred@moodle.com>
2016-09-02 17:01:19 +0800

    Merging both user generating scripts

--- mdk/scripts/users.php
+++ mdk/scripts/users.php
@@ -8,62 +8,48 @@ require(dirname(__FILE__).'/config.php');
 require_once($CFG->libdir . '/filelib.php');
 require_once($CFG->libdir . '/gdlib.php');
 
-// True to download a gravatar.
+// True to download an avatar.
 define('MDK_AVATAR', true);
 
-$data = "s1,test,Eric,Cartman,s1@example.com
-s2,test,Stan,Marsh,s2@example.com
-s3,test,Kyle,Broflovski,s3@example.com
-s4,test,Kenny,McCormick,s4@example.com
-s5,test,Butters,Stotch,s5@example.com
-s6,test,Clyde,Donovan,s6@example.com
-s7,test,Jimmy,Valmer,s7@example.com
-s8,test,Timmy,Burch,s8@example.com
-s9,test,Wendy,Testaburger,s9@example.com
-s10,test,Bebe,Stevens,s10@example.com
-t1,test,Herbert,Garrison,t1@example.com
-t2,test,Sheila,Brovslovski,t2@example.com
-t3,test,Liane,Cartman,t3@example.com
-m1,test,Officer,Barbady,m1@example.com
-m2,test,Principal,Victoria,m2@example.com
-m3,test,Randy,Marsh,m3@example.com";
-$users = explode("\n", $data);
+// Random data.
+$CITIES    = ['Perth', 'Brussels', 'London', 'Johannesburg', 'New York', 'Paris', 'Tokyo', 'Manila', 'São Paulo'];
+$COUNTRIES = ['AU', 'BE', 'UK', 'SA', 'US', 'FR', 'JP', 'PH', 'BR'];
+$DEPARTMENTS = ['Marketing', 'Development', 'Business', 'HR', 'Communication', 'Management'];
+
+// User generator.
+$generator = new mdk_randomapi_users_generator();
 
 // Create all the users.
-foreach ($users as $user) {
-    if (empty($user)) {
+foreach ($generator->get_users() as $user) {
+    if (empty($user) || empty($user->username)) {
         continue;
     }
-    $user = explode(',', $user);
-    if ($DB->record_exists('user', array('username' => $user[0], 'deleted' => 0))) {
+    if ($DB->record_exists('user', array('username' => $user->username, 'deleted' => 0))) {
         continue;
     }
 
-    mtrace('Creating user ' . $user[0]);
-    $u = create_user_record($user[0], $user[1]);
-    $u->firstname = $user[2];
-    $u->lastname = $user[3];
-    $u->email = $user[4];
-    $u->city = 'Perth';
-    $u->country = 'AU';
+    $locationindex = array_rand($CITIES);
+
+    mtrace('Creating user ' . $user->username);
+    $u = create_user_record($user->username, $user->password);
+    $u->firstname = $user->firstname;
+    $u->lastname = $user->lastname;
+    $u->email = $user->email;
+    $u->city = !empty($user->city) ? $user->city : $CITIES[$locationindex];
+    $u->country = !empty($user->country) ? $user->country : $COUNTRIES[$locationindex];
     $u->lang = 'en';
-    $u->description = 'Who\'s your daddy?';
+    $u->description = '';
     $u->url = 'http://moodle.org';
     $u->idnumber = '';
     $u->institution = 'Moodle HQ';
-    $u->department = 'Rock on!';
+    $u->department = $DEPARTMENTS[array_rand($DEPARTMENTS)];
     $u->phone1 = '';
     $u->phone2 = '';
     $u->address = '';
 
     // Adds an avatar to the user. Will slow down the process.
-    if (MDK_AVATAR) {
-        $params = array(
-            'size' => 160,
-            'force' => 'y',
-            'default' => 'wavatar'
-        );
-        $url = new moodle_url('http://www.gravatar.com/avatar/' . md5($u->id . ':' . $u->username), $params);
+    if (MDK_AVATAR && !empty($user->pic)) {
+        $url = new moodle_url($user->pic);
 
         // Temporary file name
         if (empty($CFG->tempdir)) {
@@ -86,4 +72,98 @@ foreach ($users as $user) {
     }
 
     $DB->update_record('user', $u);
+}
+
+// Fix admin user.
+$admin = $DB->get_record('user', array('username' => 'admin'));
+if ($admin && empty($admin->email)) {
+    $user = get_random_user();
+    mtrace('Fill admin user\'s email');
+    $admin->email = 'admin@example.com';
+    $DB->update_record('user', $admin);
+}
+
+/**
+ * Users generator.
+ */
+class mdk_users_generator {
+
+    protected $users;
+
+    public function __construct() {
+        $this->users = $this->generate_users();
+    }
+
+    public function generate_users() {
+        $data = "s1,test,Eric,Cartman,s1@example.com
+        s2,test,Stan,Marsh,s2@example.com
+        s3,test,Kyle,Broflovski,s3@example.com
+        s4,test,Kenny,McCormick,s4@example.com
+        s5,test,Butters,Stotch,s5@example.com
+        s6,test,Clyde,Donovan,s6@example.com
+        s7,test,Jimmy,Valmer,s7@example.com
+        s8,test,Timmy,Burch,s8@example.com
+        s9,test,Wendy,Testaburger,s9@example.com
+        s10,test,Bebe,Stevens,s10@example.com
+        t1,test,Herbert,Garrison,t1@example.com
+        t2,test,Sheila,Brovslovski,t2@example.com
+        t3,test,Liane,Cartman,t3@example.com
+        m1,test,Officer,Barbady,m1@example.com
+        m2,test,Principal,Victoria,m2@example.com
+        m3,test,Randy,Marsh,m3@example.com";
+
+        $id = 3;
+        $urlparams = array(
+            'size' => 160,
+            'force' => 'y',
+            'default' => 'wavatar'
+        );
+        $users = array_map(function($user) use (&$id, $urlparams) {
+            $data = (object) array_combine(['username', 'password', 'firstname', 'lastname', 'email'], explode(',', trim($user)));
+            $data->pic = new moodle_url('http://www.gravatar.com/avatar/' . md5($id++ . ':' . $data->username), $urlparams);
+            return $data;
+        }, explode("\n", $data));
+        return $users;
+    }
+
+    public function get_users() {
+        return $this->users;
+    }
+
+}
+
+/**
+ * Users generator from randomuser.me.
+ */
+class mdk_randomapi_users_generator extends mdk_users_generator {
+
+    public function generate_users() {
+        $curl = new curl([
+            'CURLOPT_CONNECTTIMEOUT' => 2,
+            'CURLOPT_TIMEOUT' => 5
+        ]);
+        $json = $curl->get('https://randomuser.me/api/?inc=name,picture,location,nat&results=16');
+        $data = json_decode($json);
+        if (!$data) {
+            return parent::generate_users();
+        }
+
+        $usernames = ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 't1', 't2', 't3', 'm1', 'm2', 'm3'];
+        $users = array_map(function($user) use (&$usernames) {
+            $username = array_shift($usernames);
+            return (object) [
+                'username' => $username,
+                'password' => 'test',
+                'firstname' => ucfirst($user->name->first),
+                'lastname' => ucfirst($user->name->last),
+                'email' => $username . '@example.com',
+                'city' => ucfirst($user->location->city),
+                'country' => $user->nat,
+                'pic' => $user->picture->large
+            ];
+        }, $data->results);
+
+        return $users;
+    }
+
 }
