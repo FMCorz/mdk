c853eb2e1220fb334df658fd26cffae31b57ebdc
David Monllao <david.monllao@gmail.com>
2017-02-06 09:40:35 +0100

    New script to set up a clone for testing against the security repo.

---
+++ mdk/scripts/setupsecurity.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Script to setup a clone for testing against the security repository
+#
+# It removes existing origin remote and adds the security repository in
+# read-only mode. It also adds a git hook to prevent pushes. The purpose is to
+# prevent as much as possible that security issues are released to public
+# repositories.
+
+set -e
+
+GIT=`mdk config show git`
+REPOURL=`mdk config show remotes.security`
+ORIGINREMOTE=`mdk config show upstreamRemote`
+
+# Remove origin remote.
+echo "Deleting current origin remote..."
+$GIT remote rm $ORIGINREMOTE
+
+echo "Adding security repository remote as origin..."
+${GIT} remote add $ORIGINREMOTE $REPOURL
+
+# Pushes to an unexisting URL.
+${GIT} remote set-url --push $ORIGINREMOTE no-pushes-allowed
+
+
+# Git hook to prevent all pushes in case people is adding other remotes anyway.
+content="#!/bin/sh
+
+echo \"Sorry, pushes are not allowed. This clone is not supposed to be used
+to push stuff as you may accidentally push security patches to public repos\"
+exit 1"
+
+hookfile=".git/hooks/pre-push"
+
+if [ -f "$hookfile" ]; then
+    existingcontent=$(cat $hookfile)
+
+    if [[ "$content" != "$existingcontent" ]]; then
+        # Error if there is a hook we don't know about.
+        echo "Error: Security repository setup adds a pre-push hook to "\
+        "prevent accidental pushes to public repositories. You already have a "\
+        "pre-push hook, please delete it or back it up and merge it once "\
+        "security repository setup concluded."
+        exit 1
+    fi
+else
+    # Create the file.
+    echo "Adding a git hook to prevent pushes from this clone..."
+    cat > $hookfile << EOL
+$content
+EOL
+fi
+
+exit 0
