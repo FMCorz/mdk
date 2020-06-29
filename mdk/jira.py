cee7c793e89958432812e96ad92c91487e3a2c2b
Jake Dallimore <jake@moodle.com>
2020-06-29 15:32:57 +0800

    Fix TypeError when downloading patch file on python3
    Fixes FMCorz/mdk#195

--- mdk/jira.py
+++ mdk/jira.py
@@ -108,7 +108,7 @@ class Jira(object):
 
         data = r.text
         if len(data) > 0:
-            f = open(dest, 'w')
+            f = open(dest, 'wb')
             f.write(data.encode('utf8'))
             f.close()
 
