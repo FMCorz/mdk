7d1a1ecbb560baba0bb2e17bfa1c4c29578a50c8
Dongsheng Cai <d@tux.im>
2021-06-15 11:38:29 +1000

    feature: show jira comments
     - add ability to pull comments from jira
     - highlight jira text

---
+++ mdk/ansi_escape_codes.py
@@ -0,0 +1,12 @@
+AnsiCodes = {
+    'RED': '\033[31m',
+    'MAGENTA': '\u001b[35m',
+    'YELLOW': '\033[33m',
+    'GRAY': '\033[90m',
+    'BLUE': '\033[94m',
+    'CYAN': '\033[96m',
+    'GREEN': '\033[92m',
+    'RESET': '\033[0m',
+    'BOLD': '\033[1m',
+    'UNDERLINE': '\033[4m',
+}
